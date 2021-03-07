60007ef2a07a96a1b4df5b5906054b9543615ba4
Felipe Lobo <felipeblobo@gmail.com>
2021-03-07 20:03:56 -0300

    refatorando footer

---
+++ scripts/script.js
@@ -0,0 +1,10 @@
+const imagens = document.getElementsByTagName('img');
+
+// const linksInternos = document.querySelectorAll('["href=^#"]');
+
+const primeiroH2 = document.querySelector('.animalsDescription h2');
+
+const todososP = document.querySelectorAll('p');
+const ultimoP = todososP[todososP.length - 1];
+
+const seisimagens = document.querySelectorAll('img[src^="http"]')    ;
