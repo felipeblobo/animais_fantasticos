afec27a0673360b8b46251b5c6b4a34c90694997
Felipe Lobo <felipeblobo@gmail.com>
2021-03-13 22:02:37 -0300

    Desenvolvimento de funcionalidade initAccordion

--- scripts/script.js
+++ scripts/script.js
@@ -1,20 +1,38 @@
-const tabMenu = document.querySelectorAll('.jsTabMenu li');
-const tabContent = document.querySelectorAll('.jsTabContent section');
+(function initTabNav() {
+  const tabMenu = document.querySelectorAll(".jsTabMenu li");
+  const tabContent = document.querySelectorAll(".jsTabContent section");
 
-if(tabMenu.len && tabContent.length) {
+  if (tabMenu.length && tabContent.length) {
+    tabContent[0].classList.add("active");
 
     function activeTab(index) {
-        tabContent.forEach((section) => {
-            section.classList.remove('active');
-        })
-        tabContent[index].classList.add('active');
-    };
-    
-    
-    tabMenu.forEach((item, index) => {
-        item.addEventListener('click', () => {
-            activeTab(index);
-        })
-    });
-}
+      tabContent.forEach((section) => {
+        section.classList.remove("active");
+      });
+      tabContent[index].classList.add("active");
+    }
 
+    tabMenu.forEach((item, index) => {
+      item.addEventListener("click", () => {
+        activeTab(index);
+      });
+    });
+  }
+})();
+
+
+(function initAccordion () {
+  const accordionList = document.querySelectorAll('.jsAccordion dt');
+
+  if (accordionList.length > 0) {
+    function activeAccordion(index) {
+      this.classList.toggle('active');
+      this.nextElementSibling.classList.toggle('active'); 
+    }
+  
+    accordionList.forEach((item) => {
+      item.addEventListener('click', activeAccordion)
+    })
+  }
+  
+})();
